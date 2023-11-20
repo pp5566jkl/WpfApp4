d1200f2ab5de6d503246fa3f9a80ce7861e138fe
owner <owner@C301-02>
2023-11-20 11:50:48 +0800

    加入專案檔案。

---
+++ MyDocumentViewer.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// MyDocumentViewer.xaml 的互動邏輯
+    /// </summary>
+    public partial class MyDocumentViewer : Window
+    {
+        public MyDocumentViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MyDocumentViewer myDocumentViewer = new MyDocumentViewer();
+            myDocumentViewer.Show();
+        }
+
+        private void OpenCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("OpenCommand");
+        }
+
+        private void SaveCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("SaveCommand");
+        }
+
+        private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            object property = rtbEditor.Selection.GetPropertyValue(TextElement.FontWeightProperty);
+            boldButton.IsChecked = (property is FontWeight && (FontWeight)property == FontWeights.Bold);
+
+            
+            property = rtbEditor.Selection.GetPropertyValue(TextElement.FontStyleProperty);
+            italicButton.IsChecked = (property is FontStyle && (FontStyle)property == FontStyles.Italic);
+
+            
+            property = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            underlineButton.IsChecked = (property != DependencyProperty.UnsetValue && property == TextDecorations.Underline);
+        }
+    }
+}
